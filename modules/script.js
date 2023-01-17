2aa90d1401d14195f4da351f1a1bfcb7238b123f
Andrii Ferenchuk <ferenchuk.a@gmail.com>
2023-01-17 21:16:09 +0200

    fix: give more descriptive name to function shownodelist

--- modules/script.js
+++ modules/script.js
@@ -9,11 +9,11 @@ searchBtn.addEventListener("click", async (e) => {
   await renderMovies(inpValue);
 
   // НУЖНО ПОЛУЧИТЬ ЭЛЕМЕНТЫ, КОТОРЫЕ ПОЯВЯТСЯ ПОСЛЕ ВЫПОЛНЕНИЯ ФЦНКЦИИ В ДЕВЯТОЙ СТРОКЕ, то есть нужно получить ноде-лист из хтмл єлементов, но, почему то получается пустой нод лист
-  await show();
+  await showNodeList();
 
   searchInp.value = "";
 });
 
-async function show() {
+async function showNodeListgit() {
   console.log(document.querySelectorAll("#addto-watchlist"));
 }
